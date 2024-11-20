611cf7f2f5b8689a3ef0bbe9ec9c171d473e3191
Hòa Nguyễn <hoanguyen2005hhh@gmail.com>
2024-11-20 13:38:20 +0700

    Revert "."
    This reverts commit 340e532efed1d62680ba49c9ab3c33fddaba8407.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
 import React, { useState } from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from "./Client/Home/Home";
-import About from "./Client/About/About";
-import Contact from "./Client/Contact/Contact";
-import News from "./Client/News/News";
-import LoginForm from "./Client/Login/LoginForm";
-import RegisterForm from "./Client/Register/RegisterForm";
-import "./App.css";
-import CartForm from "./Client/Cart/CartForm";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/News" element={<News />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/register" element={<RegisterForm />} />
-          <Route path="/Cart" element={<CartForm />} />
+
+        <Route path="/" element={<Home />} />
+
         </Routes>
       </BrowserRouter>
     </div>
